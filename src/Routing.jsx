573ca4b117024a09f336d9b8d60052ae24d02331
ahlilfikri <ahlilfikri49@gmail.com>
2023-10-09 20:27:20 +0700

    tester

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,16 +1,16 @@
 import { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './/pages/home/index';
+import Home from './pages/home/index.jsx';
 
 const Routing = () => {
   return (
-    <Router>
       <Fragment>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
       </Fragment>
-    </Router>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Routing from './Routing.jsx';
+import Routing from './Routing';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './index.css';
 
